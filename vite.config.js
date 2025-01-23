a1f62c4912b9fcb6c7e4fcdebdeb778feae771cb
Daniel Vallecillo <danyyva7@gmail.com>
2025-01-23 13:40:58 -0600

    actualizacion

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/Danyy-7/Challenge-aluraflix.git'
 })
